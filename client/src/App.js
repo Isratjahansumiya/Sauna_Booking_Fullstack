19d5e0250eac212a18904486d2b0837113f6ded4
isratjahan sumiya <isratjahansumiya9@gmail.com>
2022-03-16 00:50:17 +0200

    ignore bootstrap css

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from './component/Home/Home';
 import Login from './component/Login/Login';
 import Register from './component/Register/Register';
